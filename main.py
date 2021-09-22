35c595b05561165ab136e4a8adbc9b2969eed894
Amandine-2021 <90973118+Amandine-2021@users.noreply.github.com>
2021-09-21 20:33:41 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+# filename: main.py
+# Final project CSC217-Python FlaskLibrary
+# Amandine Velamala
 
+from FlaskLibraryWebsite import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(port=4996, debug=True)
